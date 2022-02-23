49c1351bef876815f7f0d13577f6b3299c7a0d77
ZUOQIN HU (Isabella) <eunicejhu@gmail.com>
2022-02-23 18:49:10 +0100

    files: add basic file structure

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// import "./index.css";
+console.log("hello,");
+
+class Config {
+  webpackConfig = "djf";
+  setWebpack(webpackConfig) {
+    webpackConfig = webpackConfig;
+  }
+}
+
+new Config();

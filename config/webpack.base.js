96382b33f3e5e39b79a92ec1b1092d50337c7b33
ZUOQIN HU (Isabella) <eunicejhu@gmail.com>
2022-02-23 18:50:20 +0100

    playground:  testing config

---
+++ config/webpack.base.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+// case1: without babel loader
+const rootDir = process.cwd();
+module.exports = {
+  entry: {
+    index: path.resolve(rootDir, "src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name]-[hash].bundle.js",
+    publicPath: "",
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(rootDir, "public/index.html"),
+      filename: "index.html",
+      chunks: ["index"],
+    }),
+  ],
+};
